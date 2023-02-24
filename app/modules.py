37ac17f2eb9e4c6c57fe93281c82972ebcb60b7f
Joel Flores <joel.flores3000@protonmail.com>
2023-02-24 11:42:16 -0400

    arreglando errores de sintaxis y usando el modulo completo

--- app/modules.py
+++ app/modules.py
@@ -1,13 +1,7 @@
 from . import personal_information as p
-from .personal_informacion import full_name as f, profesiones as pr, social_media as s, descripcion as d, services as s
+from app.personal_informacion import details_portfolio as d
 def details_portfolio():
-    json = dict()
-    json['name'] = f.data()
-    json['profesiones'] = pr.data()
-    json['social_media'] = s.data()
-    json['descriptions'] = d.data()
-    json['courses'] = p.courses
-    json['courses'] = s.data()
+    json = d()
     json['works'] = p.works
     json['journals'] = p.journals
     return json
--- app/personal_informacion/__init__.py
+++ app/personal_informacion/__init__.py
@@ -1,7 +1,10 @@
-from . import full_name as f, descripcion as d, services as s
+from . import full_name, profesiones, descripcion, services, social_media
 def details_portfolio():
     json = dict()
-    json['name'] = f.data()
-    json['descriptions'] = d.data()
-    json['courses'] = s.data()
+    json['name'] = full_name.data()
+    json['profesiones'] = profesiones.data()
+    json['social_media'] = social_media.data()
+    json['descriptions'] = descripcion.data()
+    json['courses'] = services.data()
+    
     return json
