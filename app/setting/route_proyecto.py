f3f09b58371a82fd9f1b52a37215af01a57930bf
Joel Flores <joel.flores3000@protonmail.com>
2023-02-27 22:11:06 -0400

    funcionalidad de los proyectos termiando

---
+++ app/setting/route_proyecto.py
@@ -0,0 +1,50 @@
+from flask import request, flash
+from app.db import get_db
+import os
+
+def file(proyecto, img_1, img_2):
+    # Crea la carpeta para guardar las imágenes del proyecto.
+    ruta = os.path.join(os.getcwd(), 'app', 'static', 'images', 'portfolio', proyecto)
+    os.makedirs(ruta, exist_ok=True)
+    
+    # Guarda las imágenes en la carpeta del proyecto.
+    file_name = f'{proyecto}_principal.jpg'
+    img_1.save(os.path.join(ruta, file_name))
+    file_name = f'{proyecto}_secundario.jpg'
+    img_2.save(os.path.join(ruta, file_name))
+
+def function():
+    # Obtiene una conexión y un cursor a la base de datos a través de la función get_db().
+    db, c = get_db()
+    
+    # Obtiene los datos enviados por el usuario a través del formulario web.
+    type_app = request.form['type_app']
+    titulo = request.form['titulo']
+    url = request.form['proyecto']
+    url = f'https://{url}'
+    descripcion = request.form['descripcion']
+    portada = request.files.get('formImagePortada')
+    secundario = request.files.get('formImageSecundario')
+    
+    # Verifica que los campos requeridos no estén vacíos.
+    if not type_app and not titulo and not url and not descripcion and not portada and not secundario:
+        flash('Los campos no se llenaron correctamente')
+        return
+    
+    # Guarda las imágenes del proyecto en la carpeta correspondiente.
+    file(titulo, portada, secundario)
+    
+    # Define la consulta SQL para insertar un nuevo proyecto en la base de datos.
+    sql = 'INSERT INTO trabajos(titutlo, descripcion, url_proyecto, type) VALUES(%s, %s, %s, %s)'
+    
+    # Define los valores que se van a insertar en la consulta SQL.
+    values = [titulo, descripcion, url, type_app]
+    
+    # Ejecuta la consulta SQL con los valores indicados.
+    c.execute(sql, values)
+    
+    # Guarda los cambios en la base de datos.
+    db.commit()
+    
+    # Muestra un mensaje flash para el usuario indicando que el proyecto se ha agregado con éxito.
+    flash('Proyecto agregado')
--- app/setting/routes.py
+++ app/setting/routes.py
@@ -1,6 +1,6 @@
 from . import setting
 from flask import render_template, url_for, redirect
-from . import route_images, route_profesion, route_description, route_curse
+from . import route_images, route_profesion, route_description, route_curse, route_proyecto
 
 @setting.route('/')
 def index():
@@ -21,39 +21,12 @@ def description():
     route_description.function()
     return redirect(url_for('setting.index'))
 
-@setting.route('/Proyecto', methods = ['POST'])
+@setting.route('/proyecto', methods = ['POST'])
 def Proyecto():
+    route_proyecto.function()
     return redirect(url_for('setting.index'))
 
 @setting.route('/curse', methods = ['POST'])
 def curse():
     route_curse.function()
     return redirect(url_for('setting.index'))
-
-
-
-
-
-
-
-''' @setting.route('/prueba', methods = ['GET', 'POST'])
-def prueba():
-    if request.method == 'POST':
-        #obtener todos los datos del formulario
-        titulo = request.form['titulo']
-        enlace = request.form['enlace']
-        description = request.form['description']
-        images = request.files.getlist('images')
-        #creamos la carpeta para el proyecto realizado
-        ruta = os.path.join(os.getcwd(), 'app', 'static', 'images', 'portfolio', titulo)
-        os.makedirs(ruta, exist_ok=True)
-        #contamos la cantidad de archivos
-        archivos = len(os.listdir(ruta))
-        #guardamos los archivos en la ruta especificada
-        for image in images:
-            archivos += 1
-            file_name = f'{titulo}_{archivos}.jpg'
-            image.save(os.path.join(ruta, file_name))
-        return 'recibido'
-        #return redirect(url_for('index'))
-    return render_template('formulario.html', json = data) '''
