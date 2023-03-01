bcca1e900081291dc72c35e53526246e981a4dbe
Joel Flores <joel.flores3000@protonmail.com>
2023-03-01 15:03:48 -0400

    parte de las configuraciones completa

--- app/setting/routes.py
+++ app/setting/routes.py
@@ -1,47 +1,64 @@
 from . import setting
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, request, flash
 from . import route_images, route_profesion, route_description, route_curse, route_proyecto, route_icon, route_social, route_personal_informacion
 
 @setting.route('/')
 def index():
+    flash('cualquier ajuste se reflajera directamente en la pagina principal')
     return render_template('setting/index.html')
 
-@setting.route('/images', methods = ['POST'])
-def images():
-    route_images.function()
-    return redirect(url_for('setting.index'))
-
-@setting.route('/profesion', methods = ['POST'])
-def profesion():
-    route_profesion.function()
-    return redirect(url_for('setting.index'))
-
-@setting.route('/description', methods = ['POST'])
-def description():
-    route_description.function()
-    return redirect(url_for('setting.index'))
-
-@setting.route('/proyecto', methods = ['POST'])
-def Proyecto():
-    route_proyecto.function()
-    return redirect(url_for('setting.index'))
-
-@setting.route('/curse', methods = ['POST'])
-def curse():
-    route_curse.function()
-    return redirect(url_for('setting.index'))
-
-@setting.route('/icon', methods = ['POST'])
+@setting.route('/icon', methods = ['GET', 'POST'])
 def icon():
-    route_icon.function()
-    return redirect(url_for('setting.index'))
+    if request.method == 'POST':
+        route_icon.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_icon.html')
 
-@setting.route('/social', methods = ['POST'])
-def social():
-    route_social.function()
-    return redirect(url_for('setting.index'))
-
-@setting.route('/personal', methods = ['POST'])
+@setting.route('/personal', methods = ['GET', 'POST'])
 def personal():
-    route_personal_informacion.function()
-    return redirect(url_for('setting.index'))
+    if request.method == 'POST':
+        route_personal_informacion.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_personal_informacion.html')
+
+@setting.route('/description', methods = ['GET', 'POST'])
+def description():
+    if request.method == 'POST':
+        route_description.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_description_perfil.html')
+
+@setting.route('/images', methods = ['GET', 'POST'])
+def images():
+    if request.method == 'POST':
+        route_images.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_image.html')
+
+@setting.route('/profesion', methods = ['GET', 'POST'])
+def profesion():
+    if request.method == 'POST':
+        route_profesion.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_profesion.html')
+
+@setting.route('/social', methods = ['GET', 'POST'])
+def social():
+    if request.method == 'POST':
+        route_social.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_social_media.html')
+
+@setting.route('/curse', methods = ['GET', 'POST'])
+def curse():
+    if request.method == 'POST':
+        route_curse.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_course.html')
+
+@setting.route('/proyecto', methods = ['GET', 'POST'])
+def Proyecto():
+    if request.method == 'POST':
+        route_proyecto.function()
+        return redirect(url_for('setting.index'))
+    return render_template('setting/form_works.html')
